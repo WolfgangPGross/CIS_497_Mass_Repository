f5faf97839d9a309afb682fd469a8c631233fa10
WolfgangPGross <60198468+WolfgangPGross@users.noreply.github.com>
2020-10-06 13:23:45 -0500

    Copied Over Finished Project/Assignments

---
+++ Challenge_1/Assets/Challenge 1/Scripts/ScoreManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*
+* (Wolfgang Gross)
+* (Assignment 2)
+* (Manages the Score)
+*/
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+
+    public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+        if (score >= 5)
+        {
+            won = true;
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You win! " +
+                    "Press R to try again!";
+            }
+            else
+            {
+                textbox.text = "You lose! " +
+                    "Press R to try again!";
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+}
---
+++ Prototype 1/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* (Wolfgang Gross)
+* (Assignment 2)
+* (Camera follows the player)
+*/
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
